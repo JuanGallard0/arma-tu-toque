5cc30bcd5f943421a47e41fc568fad927d3b7788
Juan Gallardo <00091813@uca.edu.sv>
2022-10-18 00:18:57 -0600

    update

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,39 +1,16 @@
-import { ApolloServer } from 'apollo-server-micro';
-import { PageConfig } from 'next';
-import { schema } from '../../graphql/schema';
-import { createContext } from '../../graphql/context';
+// lib/apollo.ts
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { relayStylePagination } from '@apollo/client/utilities';
 
-const apolloServer = new ApolloServer({
-  context: createContext,
-  schema,
+export const client = new ApolloClient({
+  uri: '/api/graphql',
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          links: relayStylePagination(),
+        },
+      },
+    },
+  }),
 });
-
-const startServer = apolloServer.start();
-
-export default async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader(
-    'Access-Control-Allow-Origin',
-    'https://studio.apollographql.com'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  if (req.method === 'OPTIONS') {
-    res.end();
-    return false;
-  }
-  await startServer;
-
-  await apolloServer.createHandler({
-    path: '/api/graphql',
-  })(req, res);
-};
-
-// // Apollo Server Micro takes care of body parsing
-export const config: PageConfig = {
-  api: {
-    bodyParser: false,
-  },
-};
