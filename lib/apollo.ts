4140a8f4053d7c805b3fc3bdd1d2484f48582b6c
Juan Gallardo <00091813@uca.edu.sv>
2022-10-19 00:28:22 -0600

    update

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -1,10 +1,11 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 import { relayStylePagination } from '@apollo/client/utilities';
 
-const URL = `${process.env.AUTH0_BASE_URL}/api/graphql`.replace("undefined","")
+//const URL = `${process.env.AUTH0_BASE_URL}/api/graphql`.replace("undefined","")
 
 export const client = new ApolloClient({
-  uri: URL,
+//  uri: URL,
+  uri:'https://arma-tu-toque.vercel.app/api/graphql',
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
