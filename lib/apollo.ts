e6624b6a941911a504f6edb389b5cd116b5dc92e
Juan Gallardo <00091813@uca.edu.sv>
2022-10-18 23:44:47 -0600

    update

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -1,8 +1,10 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 import { relayStylePagination } from '@apollo/client/utilities';
 
+const URL = `${process.env.AUTH0_BASE_URL}/api/graphql`.replace("undefined","")
+
 export const client = new ApolloClient({
-  uri: 'http://localhost:3000/api/graphql',
+  uri: URL,
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
